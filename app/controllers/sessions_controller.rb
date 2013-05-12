5017cf4ee5e5ed56b2327fd57fbcbe0df289e206
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-11 20:18:34 -0700

    adds remember token migration to user model, includes sessions helper in application controller, assigns remember token before save in user model, successful test for presense of remember token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     #gets params hash data from #new submit
     user = User.find_by_email(params[:session][:email])
     if user and user.authenticate(params[:session][:password])
-      #sign in and redirect
+      sign_in user
+      redirect_to user
     else
       #error and re-rerender new
       flash.now[:error] = 'Invalid email/password combination'
